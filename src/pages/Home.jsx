ee7797ea81c3eb24e44e9f6b03181b693d5628ed
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-19 13:40:08 +0100

    Step 7 : ajout du stae et du fetch sur le composant home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/movies')
+      .then((res) => setMovies(res.data))
+      .catch((err) => console.error(err));
+    console.log(movies);
+  }, []);
   return <div>Home</div>;
 };
 
