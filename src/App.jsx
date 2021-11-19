468b7ebee6f3f5f92c613df858e4b7b409e46bc0
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-19 11:04:50 +0100

    Step 3: ajout des routes et de leur composant page respectif

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import RemoveMe from './components/RemoveMe';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Single from './pages/Single';
+import AddMovie from './pages/AddMovie';
 
 function App() {
   return (
-    <div className="App">
-      <RemoveMe />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link exact to="/">
+          Home
+        </Link>
+        <Link exact to="/AddMovie">
+          Add a movie
+        </Link>
+        <Link to="/single/1">single 1</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AddMovie" element={<AddMovie />} />
+        <Route path="/single/:id" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AddMovie.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddMovie = () => {
+  return <div>Add a movie</div>;
+};
+
+export default AddMovie;
