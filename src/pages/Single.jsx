21699601bb1b1e77df7856dba94d0328e5ae54a4
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-25 12:05:16 +0100

    Mise en place du format du composant single JSX + CSS

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -2,18 +2,53 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+/** Import du CSS */
+import './Single.css';
+
+/** import des images */
+import {
+  action,
+  drama,
+  fantasy,
+  sciencefiction,
+  romantic,
+  comedy,
+} from '../assets/images';
+
 const Single = () => {
-  const [myMovie, setMyMovie] = useState([]);
+  const [myMovie, setMyMovie] = useState({});
   const { id } = useParams();
-  console.log('My Single Page');
+
+  const getImgSrc = {
+    action,
+    drama,
+    fantasy,
+    'science-fiction': sciencefiction,
+    romantic,
+    comedy,
+  };
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/movies/${id}`)
-      .then((res) => setMyMovie(res.data))
+      .then((res) => setMyMovie(res.data[0]))
       .catch((err) => console.error(err));
   }, []);
-  return <div>{myMovie[0].title}</div>;
+  return (
+    <div className="singleovieContainer">
+      <h2>{myMovie.title}</h2>
+      <h4>{myMovie.synopsis}</h4>
+      <img
+        src={getImgSrc[myMovie.genre]}
+        alt={myMovie.title}
+        className="movieImg"
+      />
+      <h4>
+        Genre : {myMovie.genre} Durée: {myMovie.duration}
+      </h4>
+      <h4>Année de publication: {myMovie.year}</h4>
+    </div>
+  );
 };
 
 export default Single;
