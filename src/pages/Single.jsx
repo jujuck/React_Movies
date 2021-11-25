3224399f2e77ad25cb7472364f41da1093ab65ed
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-25 11:56:37 +0100

    Step 11, mise en place d'un state et useEffect pour récupérer la donnée spécifique d'un film à partir de son id

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Single = () => {
+  const [myMovie, setMyMovie] = useState([]);
+  const { id } = useParams();
   console.log('My Single Page');
-  return <div>Single</div>;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/movies/${id}`)
+      .then((res) => setMyMovie(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+  return <div>{myMovie[0].title}</div>;
 };
 
 export default Single;
