dfae4cbddd5c5455c42d2650441a8d9305d8016e
Julien ichard WCS <julien.y.richard@gmail.com>
2021-12-10 10:34:42 +0100

    Part 2, step 21, gestion du fetch sur input user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,26 +13,47 @@ const Home = () => {
   const [genre, setGenre] = useState('');
   const [maxduration, setMaxDuration] = useState(0);
   const [minyear, setMinYear] = useState(0);
+  const [myTimeOut, setMyTimeOut] = useState();
 
-  useEffect(() => {
+  /**
+   * Fetch des données, gestion de la non multi requete
+   */
+  const getData = () => {
+    // Préparation de l'Url
     let url = 'http://localhost:5000/movies?';
     if (genre !== '') url += `genre=${genre}&`;
     if (maxduration !== 0) url += `max_duration=${maxduration}&`;
     if (minyear !== 0) url += `min_year=${minyear}`;
     axios
       .get(url)
-      .then((res) => setMovies(res.data))
+      .then((res) => {
+        setMovies(res.data);
+      })
       .catch((err) => console.error(err));
+  };
+
+  /**
+   * Gestion des inputs: on attends 0.3 sec avant la requete pour eviter le multi fetching sur saisie user
+   */
+  useEffect(() => {
+    clearTimeout(myTimeOut);
+    const id = setTimeout(() => {
+      getData();
+    }, 300);
+    setMyTimeOut(id);
   }, [genre, maxduration, minyear]);
 
+  /** Mise à jour du state MinYear */
   const onSetMinYear = (value) => {
     setMinYear(value);
   };
 
+  /** Mise à jour du state MaxDuration */
   const onSetMaxDuration = (value) => {
     setMaxDuration(value);
   };
 
+  /** Mise à jour du state Genrer */
   const onSetGenre = (value) => {
     setGenre(value);
   };
